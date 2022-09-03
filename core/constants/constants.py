bd3a6649f4352faf203cac4bb68cb7614df9c0a0
Parashara <parasharar@gmail.com>
2022-09-03 20:41:18 +0530

    adding models and restructuring

--- core/constants/constants.py
+++ core/constants/constants.py
@@ -1,3 +1,8 @@
-#this will be used for defining all constants
-SCOPES = ["https://www.googleapis.com/auth/documents", "https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/spreadsheets"]
+# this will be used for defining all constants
+from enum import Enum
+
+SCOPES = ["https://www.googleapis.com/auth/documents",
+          "https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/spreadsheets"]
 SHEET_NO = 'Sheet1'
+LANGUAGES = Enum(ENGLISH="english", KANNADA="kannada")
--- core/models/models.py
+++ core/models/models.py
@@ -1,5 +1,33 @@
-#all models and types come here
+# all models and types come here
+from core.constants.constants import LANGUAGES
+from datetime import datetime
+from dateutil.tz import gettz
 
 class Notation:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 name=None,
+                 language=LANGUAGES.ENGLISH,
+                 type=None,
+                 raga=None,
+                 tala=None,
+                 composer=None,
+                 arohanam=None,
+                 avarohanam=None,
+                 comments=None,
+                 ragaMetaData=None,
+                 notatedBy=None,
+                 reviewedBy=None,
+                 lastModified=datetime.now(tz=gettz('Asia/Kolkata'))):
+        self.name = name
+        self.language = language
+        self.type = type
+        self.raga = raga
+        self.tala = tala
+        self.composer = composer
+        self.arohanam = arohanam
+        self.avarohanam = avarohanam
+        self.comments = comments
+        self.ragaMetaData = ragaMetaData
+        self.notatedBy = notatedBy
+        self.reviewedBy = reviewedBy
+        self.lastModified = lastModified
