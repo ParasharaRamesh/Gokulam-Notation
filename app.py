bcb0d63831891756d650674c58ddec60abaafedf
Parashara <parasharar@gmail.com>
2022-10-03 19:08:34 +0530

    adding flask cors

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import logging
 from flask import Flask
+from flask_cors import CORS
 
 #NOTE: comment out the import statement from apis and the line 'api.init_app' for running experiements locally
 from apis import api
 
 app = Flask(__name__)
+CORS(app)
+
 api.init_app(app)
 
 
